13305001480db8b689abae94df601ea45d62c429
Matthew Knight <mattknight.sa@gmail.com>
2020-10-06 11:53:19 -0500

    finished prep function

--- matt_prepare.py
+++ matt_prepare.py
@@ -8,6 +8,9 @@ from sklearn.preprocessing import MinMaxScaler
 
 from sklearn.model_selection import train_test_split
 
+
+
+
 def prep_zillow_data(target):
     df = get_zillow_data()
     df = df[['calculatedfinishedsquarefeet', 'bedroomcnt', 'bathroomcnt', 'taxvaluedollarcnt' ]]
@@ -30,38 +33,43 @@ def prep_zillow_data(target):
 
     scaler.fit(X_train)
 
-    X_train_scaled = scaler.transform(X_train)
-    X_validate_scaled = scaler.transform(X_validate)
-    X_test_scaled = scaler.transform(X_test)
+    X_train_scaled = pd.DataFrame(scaler.transform(X_train))
+    X_validate_scaled = pd.DataFrame(scaler.transform(X_validate))
+    X_test_scaled = pd.DataFrame(scaler.transform(X_test))
 
-    return y_train, y_validate, y_test, X_test_scaled, X_validate_scaled, X_test_scaled
+
+    X_train_scaled.rename(columns={0: 'Unit_Sq_ft', 1: 'Bedroom_Count', 2: 'Bathroom_Count'}, inplace=True)
+    X_validate_scaled.rename(columns={0: 'Unit_Sq_ft', 1: 'Bedroom_Count', 2: 'Bathroom_Count'}, inplace=True)
+    X_test_scaled.rename(columns={0: 'Unit_Sq_ft', 1: 'Bedroom_Count', 2: 'Bathroom_Count'}, inplace=True)
+
+    return y_train, y_validate, y_test, X_train_scaled, X_validate_scaled, X_test_scaled
     
 
 
 
-def train_validate_test(df, target):
+# def train_validate_test(df, target):
    
-    train_validate, test = train_test_split(df, test_size=.2, random_state=123)
+#     train_validate, test = train_test_split(df, test_size=.2, random_state=123)
 
-    train, validate = train_test_split(train_validate, test_size=.3, random_state=123)
+#     train, validate = train_test_split(train_validate, test_size=.3, random_state=123)
 
-    X_train = train.drop(columns=[target])
-    y_train = train[target]
+#     X_train = train.drop(columns=[target])
+#     y_train = train[target]
     
-    X_validate = validate.drop(columns=[target])
-    y_validate = validate[target]
+#     X_validate = validate.drop(columns=[target])
+#     y_validate = validate[target]
     
-    X_test = test.drop(columns=[target])
-    y_test = test[target]
+#     X_test = test.drop(columns=[target])
+#     y_test = test[target]
     
-    return X_train, y_train, X_validate, y_validate, X_test, y_test
+#     return X_train, y_train, X_validate, y_validate, X_test, y_test
 
-def min_max_scale(X_train, X_validate, X_test, ):
+# def min_max_scale(X_train, X_validate, X_test, ):
     
-    scaler = sklearn.preprocessing.MinMaxScaler()
+#     scaler = sklearn.preprocessing.MinMaxScaler()
 
-    scaler.fit(x_train)
+#     scaler.fit(X_train)
 
-    x_train_scaled = scaler.transform(x_train)
-    x_validate_scaled = scaler.transform(x_validate)
-    x_test_scaled = scaler.transform(x_test)
+#     x_train_scaled = scaler.transform(X_train)
+#     x_validate_scaled = scaler.transform(X_validate)
+#     x_test_scaled = scaler.transform(X_test)
