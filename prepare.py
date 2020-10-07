5783cd431bfcb01f70f7c4da93932305f7e8fd09
Samuel Davila <samuelrdavila@gmail.com>
2020-10-06 20:39:22 -0500

    updated

--- prepare.py
+++ prepare.py
@@ -1,8 +1,7 @@
 import pandas as pd
 import numpy as np
 import sklearn
-import matt_acquire
-from matt_acquire import get_zillow_data
+from acquire import get_zillow_data
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import train_test_split
 
@@ -45,7 +44,11 @@ def prep_zillow_data(target):
     X_train_scaled.rename(columns = {0: 'unit_sq_feet', 1: 'bedroom_count', 2: 'bathroom_count', 3: 'lot_size_sq_feet' }, inplace=True)
     X_validate_scaled.rename(columns = {0: 'unit_sq_feet', 1: 'bedroom_count', 2: 'bathroom_count',  3: 'lot_size_sq_feet'}, inplace=True)
     X_test_scaled.rename(columns = {0: 'unit_sq_feet', 1: 'bedroom_count', 2: 'bathroom_count',  3: 'lot_size_sq_feet'}, inplace=True)
-
+    
+    X_train.rename(columns = {'calculatedfinishedsquarefeet' : 'unit_sq_feet',	'bedroomcnt' : 'bedroom_count',	'bathroomcnt' : 'bathroom_count', 'lotsizesquarefeet' : 'lot_size_sq_feet' }, inplace=True)
+    X_validate.rename(columns = {'calculatedfinishedsquarefeet' : 'unit_sq_feet',	'bedroomcnt' : 'bedroom_count',	'bathroomcnt' : 'bathroom_count', 'lotsizesquarefeet' : 'lot_size_sq_feet' }, inplace=True)
+    X_test.rename(columns = {'calculatedfinishedsquarefeet' : 'unit_sq_feet',	'bedroomcnt' : 'bedroom_count',	'bathroomcnt' : 'bathroom_count', 'lotsizesquarefeet' : 'lot_size_sq_feet' }, inplace=True)
+    
     y_train.rename(columns = {'taxvaluedollarcnt': 'tax_dollar_value'}, inplace=True)
     y_validate.rename(columns = {'taxvaluedollarcnt': 'tax_dollar_value'}, inplace=True)
     y_test.rename(columns = {'taxvaluedollarcnt': 'tax_dollar_value'}, inplace=True)
