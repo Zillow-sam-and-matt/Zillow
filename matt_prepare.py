699c009b5bfa2acb3014fc28c6fb6d2422c12e1a
Matthew Knight <mattknight.sa@gmail.com>
2020-10-06 20:31:22 -0500

    modeling

--- matt_prepare.py
+++ matt_prepare.py
@@ -21,18 +21,19 @@ def prep_zillow_data(target):
     train, validate = train_test_split(train_validate, test_size=.3, random_state=123)
 
     X_train = train.drop(columns=[target])
-    y_train = train[target]
+    y_train = train[[target]]
     
     X_validate = validate.drop(columns=[target])
-    y_validate = validate[target]
+    y_validate = validate[[target]]
     
     X_test = test.drop(columns=[target])
-    y_test = test[target]
+    y_test = test[[target]]
     
     scaler = sklearn.preprocessing.MinMaxScaler()
 
     scaler.fit(X_train)
 
+   
     X_train_scaled = pd.DataFrame(scaler.transform(X_train))
     X_validate_scaled = pd.DataFrame(scaler.transform(X_validate))
     X_test_scaled = pd.DataFrame(scaler.transform(X_test))
@@ -42,7 +43,7 @@ def prep_zillow_data(target):
     X_validate_scaled.rename(columns={0: 'unit_sq_feet', 1: 'bedroom_count', 2: 'bathroom_count',  3: 'lot_size_sq_feet'}, inplace=True)
     X_test_scaled.rename(columns={0: 'unit_sq_feet', 1: 'bedroom_count', 2: 'bathroom_count',  3: 'lot_size_sq_feet'}, inplace=True)
 
-    return y_train, y_validate, y_test, X_train_scaled, X_train, X_validate, X_test, X_validate_scaled, X_test_scaled
+    return train, y_train, y_validate, y_test, X_train_scaled, X_train, X_validate, X_test, X_validate_scaled, X_test_scaled
     
 
 
