3ac95c4ed52e4a83345eea43f98cd58a9b57f5a5
Samuel Davila <samuelrdavila@gmail.com>
2020-12-18 12:47:01 -0600

    updated acquire to only pull specified columns

--- acquire.py
+++ acquire.py
@@ -14,16 +14,13 @@ def get_connection(db, user=user, host=host, password=password):
 # function to get data for preparation, exploration, modeling
 def get_zillow_data():
     """
-    Function connects to the data science database and returns a data frame containing Zillow data for properties whose last transaction was in May through June 2017. This data will be used to find key drivers of property value and to create a model that predicts property value.
+    Function connects to the data science database and returns a data frame containing Zillow data for propertys whose last transaction was in May through June 2017.
     """
     # SQL query string
-    sql_query = "SELECT * FROM properties_2017 JOIN predictions_2017 on predictions_2017.parcelid = properties_2017.parcelid WHERE unitcnt = 1 AND transactiondate BETWEEN '2017-05-01' AND '2017-06-30'"
+    sql_query = "SELECT properties_2017.id, properties_2017.parcelid, airconditioningtypeid, architecturalstyletypeid, basementsqft, bathroomcnt, bedroomcnt, buildingclasstypeid,buildingqualitytypeid, calculatedbathnbr, decktypeid,finishedfloor1squarefeet, calculatedfinishedsquarefeet,finishedsquarefeet12, finishedsquarefeet13, finishedsquarefeet15,finishedsquarefeet50, finishedsquarefeet6, fips, fireplacecnt,fullbathcnt, garagecarcnt, garagetotalsqft, hashottuborspa,heatingorsystemtypeid, latitude, longitude, lotsizesquarefeet,poolcnt, poolsizesum, pooltypeid10, pooltypeid2, pooltypeid7,propertycountylandusecode, propertylandusetypeid,propertyzoningdesc, rawcensustractandblock, regionidcity,regionidcounty, regionidneighborhood, regionidzip, roomcnt,storytypeid, threequarterbathnbr, typeconstructiontypeid,unitcnt, yardbuildingsqft17, yardbuildingsqft26, yearbuilt,numberofstories, fireplaceflag, structuretaxvaluedollarcnt,taxvaluedollarcnt, assessmentyear, landtaxvaluedollarcnt,taxamount, taxdelinquencyflag, taxdelinquencyyear,censustractandblock, predictions_2017.id, predictions_2017.parcelid, logerror, transactiondate FROM properties_2017 JOIN predictions_2017 on predictions_2017.parcelid = properties_2017.parcelid WHERE unitcnt = 1 AND transactiondate BETWEEN '2017-05-01' AND '2017-06-30'"
     
-    # creates dataframe using data from DS database, Zillow table
+    # creates dataframe using data from DS database
     df = pd.read_sql(sql_query, get_connection('zillow'))
     
-    # writes data to csv file for future use
-    df.to_csv('zillow_df.csv')
-    
-    # returns data frame
+    # returns DF 
     return df
